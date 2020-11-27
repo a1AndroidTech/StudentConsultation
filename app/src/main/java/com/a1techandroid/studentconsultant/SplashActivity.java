83b35e3589bfd4b32e9e5f27e1d8fe23e9d798a2
Haroon Shoukat <haroon5507@gmail.com>
2020-11-27 19:15:19 +0500

    this is first commit

---
+++ app/src/main/java/com/a1techandroid/studentconsultant/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.a1techandroid.studentconsultant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        auth=FirebaseAuth.getInstance();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (auth.getCurrentUser() != null) {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                }else {
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    finish();
+                }            }
+        }, 3000);
+    }
+}
